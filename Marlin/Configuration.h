95230009aaad37efbb5f5f645fcea27d4aeb00a4
brooskib-cmyk <brooskib@gmail.com>
2025-12-23 18:40:06 -0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -87,7 +87,8 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_CREALITY_V4
+#define MOTHERBOARD BOARD_AQUILA_S2_N32   
+// or whatever the fork calls the S2 N32 board
 #endif
 
 /**
